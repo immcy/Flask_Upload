b82c869b0a5846f087ef3c6c79dd734cf11ef244
wt <229589252@qq.com>
2018-02-06 11:23:26 +0800

    noting

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, render_template, redirect, session, flash
+import os
+from flask import Flask, request, render_template, redirect,\
+    session, flash, abort
 from werkzeug.utils import secure_filename
 
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'string'
+
+app.config.from_pyfile('settings.py', silent=False)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'apk'])
 
@@ -10,6 +14,7 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'apk'])
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -27,12 +32,24 @@ def index():
             flash('file type not support')
             return redirect('/')
 
-
-    else:
-        filename = None
-    print(filename)
     filename = session.get('filename')
+    print(filename)
     return render_template('test.html', filename=filename)
 
 
-app.run(debug=True)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username is None or password is None:
+            abort(400)  # 用户名或者密码为空
+        elif username != "wt" or password != "123":
+            return render_template('login.html')
+        else:
+            return redirect('/')
+    return render_template('login.html')
+
+
+if __name__ == "__main__":
+    app.run()
